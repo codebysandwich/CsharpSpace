7f52c801270a4711458fb674daa91bcd828edff8
unknown <zhiming_cai@cas-pe.com>
2021-07-08 17:24:36 +0800

    console learn proj

---
+++ ConsoleApps/FileTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTest
+{
+    class Program
+    {
+        static bool IsEmpty(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                return true;
+            }
+            else
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                if (fileInfo.Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string fileName = "F:\\VsCode\\hello.txt";
+            Console.WriteLine(IsEmpty(fileName));
+            Console.ReadKey();
+        }
+    }
+}
