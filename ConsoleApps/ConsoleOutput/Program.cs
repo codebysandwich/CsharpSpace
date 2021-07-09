0d218c0765320ba89b6390c00a12f8103ece4ac1
unknown <zhiming_cai@cas-pe.com>
2021-07-09 10:26:10 +0800

    update pow

--- ConsoleApps/ConsoleOutput/Program.cs
+++ ConsoleApps/ConsoleOutput/Program.cs
@@ -11,8 +11,12 @@ namespace ConsoleOutput
         static void Main(string[] args)
         {
             Console.WriteLine("hello world!");
-
+            Console.WriteLine(Add(2, 3));
             Console.ReadKey();
+        }
+        static double Add(double x, double y)
+        {
+            return Math.Pow(x, y);
         }
     }
 }
