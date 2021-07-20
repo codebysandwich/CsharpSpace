87b68c38c2ffc11fff5076dd54db84c3ba24ec4f
codebysandwich <122079260@qq.com>
2021-07-20 18:16:12 +0800

    submit HeyYou

---
+++ EssentialCShape/HeyYou/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HeyYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            Console.WriteLine("Hey you");
+
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine($"hey {firstName} {lastName}");
+        }
+    }
+}
