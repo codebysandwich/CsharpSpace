d0b81f7ca842e372447a475ca9cd9d98e03a05f9
sandwich <122079260@qq.com>
2021-07-28 23:21:38 +0800

    Submit Delegate & Event

---
+++ Books/EssentialCShape/DelegateAndEvent/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/// <summary>
+/// 讲解委托与事件
+/// </summary>
+namespace DelegateAndEvent
+{
+    // 定义一个委托
+    public delegate void SayHello(string name); 
+    class Program
+    {
+        // 事件-限制了的委托
+        public static event SayHello SayEnv;
+        static void Main(string[] args)
+        {
+            // 注册一个委托
+            SayHello Say = new SayHello(Sayhello);
+            // 链式注册
+            Say += Godbey;
+            // lambda表达式
+            Say += (string name) => {
+                Console.WriteLine($"hola, {name}");
+            };
+            Say("sandwich");
+
+            // 通过事件实现
+            SayEnv += Call;
+            SayEnv("事件触发");
+            
+        }
+
+        #region 注册的函数事件
+        static void Sayhello(string name)
+        {
+            Console.WriteLine($"hello, {name}");
+        }
+
+        static void Godbey(string name)
+        {
+            Console.WriteLine($"godbey, {name}");
+        }
+        static void Call(string name)
+        {
+            Console.WriteLine($"事件触发，{name}");
+        }
+        #endregion
+    }
+}
