fd3721a90c6e76ebd92f9b110a3226bcc4ccbd34
codebysandwich <122079260@qq.com>
2021-08-02 00:17:04 +0800

    submit xor proj

---
+++ ConsoleApps/XOR/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(XOR(a, b));
+            a = true;
+            b = true;
+            Console.WriteLine(XOR(a, b));
+            a = false;
+            b = false;
+            Console.WriteLine(XOR(a, b));
+            a = false;
+            b = true;
+            Console.WriteLine(XOR(a, b));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 使用与或非逻辑实现XOR
+        /// </summary>
+        /// <param name="x">输入1</param>
+        /// <param name="y">输入2</param>
+        /// <returns></returns>
+        static bool XOR(bool x, bool y)
+        {
+            return (!(x && y)) && (x || y);
+        }
+    }
+}
