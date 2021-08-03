7fd4f5e1cc39412e99773ca8c0fd7db74eebb028
sandwich <122079260@qq.com>
2021-08-03 17:02:46 +0800

    submit Extension for class string

---
+++ Books/EssentialCShape/Extension/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "this is an extension sample!";
+            Console.WriteLine("s have {0} words", s.CountWord());
+        }
+    }
+    public static class StrExtension
+    {
+        /// <summary>
+        /// 为string类型拓展接口,计算单词个数
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int CountWord(this string str)
+        {
+            return str.Split(new char[] {' ', ',', '!', '?'},
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
