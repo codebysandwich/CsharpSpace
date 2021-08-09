7c2e69f6926a82c4e556863b20efb621f0298443
codebysandwich <122079260@qq.com>
2021-08-09 23:58:28 +0800

    submit try-catch

---
+++ ConsoleApps/TryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number=0;
+            // 标记是否成功
+            bool iserror = false;
+            Console.WriteLine("请输入需要转换的数字:");
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("输入的数据格式无法转化为整形");
+            }
+            catch (Exception e)
+            {
+                iserror = true;
+                Console.WriteLine(e);
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("测试完成!");
+            }
+            if (!iserror)
+            {
+                Console.WriteLine(number*2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
