4f73dad39801ab15deabffa17e6882ff1c0f2bfe
codebysandwich <122079260@qq.com>
2021-08-15 23:00:19 +0800

    submit Loop

---
+++ ConsoleApps/Loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 测试continue和break
+            TestContinueAndBreak();
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 测试continue和break
+        /// </summary>
+        static void TestContinueAndBreak()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                    continue;
+                else if (i == 7)
+                    break;
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
