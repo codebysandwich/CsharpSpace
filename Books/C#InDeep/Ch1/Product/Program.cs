b7b90619f0c6c727af1dde1f61a8f0227d08e68d
codebysandwich <122079260@qq.com>
2021-08-18 23:12:07 +0800

    submit C# in deep

---
+++ Books/C#InDeep/Ch1/Product/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = Product.GetSampleProducts();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Product
+    {
+        string name;
+        decimal price;
+        /*return name可以省略*/
+        public string Name { get { return name; } }
+        public decimal Price { get { return price; } }
+
+        public Product(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        /// <summary>
+        /// 静态方法：生产数据模板 C#1
+        /// </summary>
+        /// <returns></returns>
+        public static ArrayList GetSampleProducts()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(new Product("West Side Story", 9.99m));
+            list.Add(new Product("Assassins", 14.99m));
+            list.Add(new Product("Frogs", 13.99m));
+            list.Add(new Product("Sweeney Todd", 10.99m));
+            return list;
+        }
+
+        #region C# 2
+        /// <summary>
+        /// List<Product> 等价与 ArrayList [Product], 无法重载
+        /// </summary>
+        /// <returns></returns>
+        //public static List<Product> GetSampleProducts()
+        //{
+        //    List<Product> list = new List<Product>();
+        //    list.Add(new Product("West Side Story", 9.99m));
+        //    list.Add(new Product("Assassins", 14.99m));
+        //    list.Add(new Product("Frogs", 13.99m));
+        //    list.Add(new Product("Sweeney Todd", 10.99m));
+        //    return list;
+        //}
+        #endregion
+        /// <summary>
+        /// 重写ToString方法
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", name, price);
+        }
+    }
+}
