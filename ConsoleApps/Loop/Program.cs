35ebbea4acb9362b537976b10d7188127fe7de7d
codebysandwich <122079260@qq.com>
2021-08-22 22:22:26 +0800

    update Loop

--- ConsoleApps/Loop/Program.cs
+++ ConsoleApps/Loop/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Loop
 {
+    /// <summary>
+    /// 展示C#中的循环语法
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -16,6 +19,11 @@ namespace Loop
             //CalScore();
 
             Console.WriteLine("100以内的素数和是：{0}", SumPrime(100));
+
+            Console.WriteLine(SumNarcissistic());
+
+            Console.WriteLine("=============================================");
+            Multi();
 
             Console.ReadKey();
         }
@@ -86,15 +94,65 @@ namespace Loop
             }
             return true;
         }
+        /// <summary>
+        /// 求2-num之间素数的和
+        /// </summary>
+        /// <param name="num">最大的范围</param>
+        /// <returns></returns>
         static int SumPrime(int num)
         {
             int sum = 0;
-            for (int i = 2; i<=100; i++)
+            for (int i = 2; i<=num; i++)
             {
                 if(IsPrime(i))
                     sum += i;
             }
             return sum;
+        }
+        /// <summary>
+        /// 水仙花数:一个 n 位正整数，它的每个位上的数字的 n 次幂之和 等于它本身
+        /// 这里n取3
+        /// </summary>
+        /// <param name="num">100~999的三位正整数</param>
+        /// <returns></returns>
+        static bool IsNarcissistic(int num)
+        {
+            // 个位数
+            int x = num % 10;
+            // 十位数
+            int y = num / 10 % 10;
+            // 百位数
+            int z = num / 100;
+            return x * x * x + y * y * y + z * z * z == num;
+
+        }
+        /// <summary>
+        /// 求100-999之内水仙花数的和
+        /// </summary>
+        /// <returns></returns>
+        static int SumNarcissistic()
+        {
+            int sum = 0;
+            for (int i = 100; i <= 999; i++)
+            {
+                if (IsNarcissistic(i))
+                    sum += i;
+            }
+            return sum;
+        }
+        /// <summary>
+        /// 打印乘法口诀
+        /// </summary>
+        static void Multi()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j}x{i}={i*j}\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
