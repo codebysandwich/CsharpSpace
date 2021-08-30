b1f1bd9021cb28da14faf3fc95b72bda936ace60
codebysandwich <122079260@qq.com>
2021-08-31 01:02:06 +0800

    submit Params

---
+++ ConsoleApps/Params/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Sum(3, 5, 8);
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 演示搜集参数
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="scores"></param>
+        /// <returns></returns>
+        static int Sum(params int[] scores)
+        {
+            int sum = 0;
+            foreach (var item in scores)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
