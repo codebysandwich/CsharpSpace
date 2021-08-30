418e6aaf3905f4fef6b517b00505b3fb223444cb
codebysandwich <122079260@qq.com>
2021-08-31 01:02:20 +0800

    update ref and out

--- ConsoleApps/RefAndOut/Program.cs
+++ ConsoleApps/RefAndOut/Program.cs
@@ -11,12 +11,31 @@ namespace RefAndOut
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"D:\Code\1.Repos\CsharpSpace\MyWork\IEM\IEM");
+            #region 递归获取文件夹下的文件
+            DirectoryInfo dir = new DirectoryInfo(@"..\..\..\..\MyWork\IEM\IEM");
             List<string> list = new List<string>();
             GetFiles(dir, list);
             foreach (var fileName in list)
             {
                 Console.WriteLine(fileName);
+            }
+            #endregion
+
+            int[] nums = { 3, 1, 9 };
+            int max, min, sum;
+            double mean;
+            if (Cal(nums, out max, out min, out sum, out mean))
+            {
+                Console.WriteLine("最大值:{0}, 最小值:{1}, 和:{2}, 均值:{3:N2}", max,
+                    min, sum, mean);
+            }
+
+            Console.WriteLine("===============================================");
+            nums = new int[] { 3, 8, 1 };
+            ShowRef(ref nums);
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
             }
 
             Console.ReadKey();
@@ -37,6 +56,52 @@ namespace RefAndOut
             {
                 GetFiles(item, fileList);
             }
+        }
+        /// <summary>
+        /// 判断和是否不为零
+        /// 获取最大最小值，和与均值
+        /// </summary>
+        /// <param name="nums">数组</param>
+        /// <param name="max">最大值</param>
+        /// <param name="min">最小值</param>
+        /// <param name="sum">和</param>
+        /// <param name="mean">均值</param>
+        /// <returns></returns>
+        static bool Cal(int[] nums, out int max, out int min, out int sum, out double mean)
+        {
+            bool isCheck = false;
+            sum = 0;
+            max = nums[0];
+            min = nums[0];
+            mean = 0;
+            foreach (var item in nums)
+            {
+                sum += item;
+                min = min > item ? item : min;
+                max = max < item ? item : max;
+            }
+            if (sum == 0)
+            {
+                return isCheck;
+            }
+            else
+            {
+                isCheck = true;
+                mean = 1.0 * sum / nums.Length;
+                return isCheck;
+            }
+        }
+
+        static void ShowRefence(int[] nums)
+        {
+            nums[0] = -99;
+            nums = new int[] { 0, 1, 2 };
+        }
+
+        static void ShowRef(ref int[] nums)
+        {
+            nums[0] = -99;
+            nums = new int[] { 0, 1, 2 };
         }
     }
 }
