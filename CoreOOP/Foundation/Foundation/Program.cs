aa1858a99b61e30fa293733fb7564dd291606fd4
sandwich <122079260@qq.com>
2021-09-22 17:00:50 +0800

    update Math class

--- CoreOOP/Foundation/Foundation/Program.cs
+++ CoreOOP/Foundation/Foundation/Program.cs
@@ -8,6 +8,14 @@ namespace Foundation
         {
             var customer = new PhoneCustomer("sandwich");
             Console.WriteLine("customer-> FirstName:{0}, Age:{1}", customer.FirstName, customer.Age);
+
+            #region math class
+            Math math = new Math();
+            math.Value = 2;
+            Console.WriteLine(math.GetSquare());
+            int res = Math.GetSquareOf(3);
+            Console.WriteLine($"类方法输出平方结果:{res}");
+            #endregion
         }
     }
 
@@ -20,6 +28,7 @@ namespace Foundation
         public string FirstName
         {
             get { return _firstName; }
+            // 设置属性的访问权限
             private set
             {
                 _firstName = value;
@@ -34,5 +43,17 @@ namespace Foundation
             FirstName = firstName;
         }
         public int Age { get; set; } = 42;
+    }
+
+    /// <summary>
+    /// 演示静态方法和成员方法
+    /// </summary>
+    class Math
+    {
+        public int Value { get; set; }
+        public int GetSquare() => Value * Value;
+        public static int GetSquareOf(int x) => x * x;
+        public static double GetPi() => 3.14159;
+
     }
 }
