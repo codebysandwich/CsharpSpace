ae786c4f40329428932f7c03008316f7c780b06f
sandwich <122079260@qq.com>
2021-09-26 17:49:00 +0800

    :apple: update Singleton & init

--- CoreOOP/Foundation/Foundation/Program.cs
+++ CoreOOP/Foundation/Foundation/Program.cs
@@ -13,9 +13,14 @@ namespace Foundation
             Math math = new Math();
             math.Value = 2;
             Console.WriteLine(math.GetSquare());
-            int res = Math.GetSquareOf(3);
+            // 命名参数
+            int res = Math.GetSquareOf(x:3);
             Console.WriteLine($"类方法输出平方结果:{res}");
             #endregion
+            Console.WriteLine(Singleton.Instance);
+
+            Car car = new Car("Proton Persona");
+            Console.WriteLine(car);
         }
     }
 
@@ -55,5 +60,45 @@ namespace Foundation
         public static int GetSquareOf(int x) => x * x;
         public static double GetPi() => 3.14159;
 
+    }
+
+    /// <summary>
+    /// 实现单例模式
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton s_instance;
+        private int _state;
+        private Singleton(int state)
+        {
+            _state = state;
+        }
+        public static Singleton Instance 
+        { 
+            get{return s_instance ?? (s_instance = new Singleton(42));}
+        }
+
+        public override string ToString()
+        {
+            return $"Singleton state = {_state}";
+        }
+    }
+
+    class Car
+    {
+        private string _description;
+        private uint _nWheels;
+        public Car(string description, uint nWheels=4)
+        {
+            _description = description;
+            _nWheels = nWheels;
+        }
+        public Car(string description): this(description, 5)
+        {// 此处级别高于参数默认值
+        }
+        public override string ToString()
+        {
+            return $"Car description:{_description}, nWheels:{_nWheels}";
+        }
     }
 }
